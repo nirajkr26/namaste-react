dfc5ab4f1db900ab5c4d46c3150c79cc5e4d6584
Niraj Kumar <nirajkumargupta2642006@gmail.com>
2025-09-10 13:12:56 +0000

    outlet and children routes

--- food-app/src/app.js
+++ food-app/src/app.js
@@ -6,14 +6,14 @@ import Footer from "./components/Footer";
 import About from "./components/About";
 import Contact from "./components/Contact"
 import Error from "./components/Error";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 
 
 const AppLayout = () => {
     return (
         <div className="app">
             <Header />
-            <Body />
+            <Outlet />
             <Footer />
         </div>
     );
@@ -25,16 +25,23 @@ const appRouter = createBrowserRouter([
     {
         path: "/",
         element: <AppLayout />,
-        errorElement:<Error/>,
+        children: [
+            {
+                path: "/",
+                element: <Body />
+            },
+            {
+                path: "/about",
+                element: <About />,
+            },
+            {
+                path: "/contact",
+                element: <Contact />,
+            },
+        ],
+        errorElement: <Error />,
     },
-    {
-        path: "/about",
-        element: <About />,
-    },
-    {
-        path: "/contact",
-        element: <Contact />,
-    },
+
 ])
 
 
