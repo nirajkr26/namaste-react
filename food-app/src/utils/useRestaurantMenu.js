f8f51d84879228cc23d54f8dd88b5f7d7e9a6f58
nirajkr26 <nirajkumargupta2642006@gmail.com>
2025-09-17 20:14:21 +0530

    custom hook

---
+++ food-app/src/utils/useRestaurantMenu.js
@@ -0,0 +1,21 @@
+import { MENU_API } from "./contents";
+import { useState, useEffect } from "react";
+
+const useRestaurantMenu = (resId) => {
+    const [resInfo,setResInfo]=useState(null);
+
+    useEffect(()=>{
+        fetchData();
+    },[]);
+
+    const fetchData=async()=>{
+        const data=await fetch(MENU_API+resId);
+        const json=await data.json();
+        console.log(json.data)
+        setResInfo(json.data);
+    }
+
+    return resInfo;
+}
+
+export default useRestaurantMenu;
