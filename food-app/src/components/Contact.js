4dce3d27135ab9686908d8979a7ae2e3b841bfe9
nirajkr26 <nirajkumargupta2642006@gmail.com>
2025-10-10 11:52:52 +0530

    testing using jest

--- food-app/src/components/Contact.js
+++ food-app/src/components/Contact.js
@@ -1,6 +1,15 @@
-const Contact=()=>{
+const Contact = () => {
     return (
-        <div className="text-2xl font-bold">Contact</div>
+        <div>
+
+            <h1 className="text-2xl font-bold">Contact</h1>
+
+            <form>
+                <input type="text" className="border border-black p-2 m-2" placeholder="name" />
+                <input type="text" className="border border-black p-2 m-2" placeholder="message" />
+                <button className="bg-gray-200 rounded-lg">Submit</button>
+            </form>
+        </div>
     )
 
 }
